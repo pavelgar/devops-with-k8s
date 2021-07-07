6ee69895f17045f4c985a051218a4e9c118cae91
Pavel Garmuyev <pavel.garmuyev@gmail.com>
2021-07-07 09:07:04 +0300

    Add status endpoint in main app

--- main_app/index.js
+++ main_app/index.js
@@ -1,14 +1,28 @@
+const express = require("express")
+
+const app = express()
+const port = process.env.PORT || 3000
+
 const genHash = () =>
+  new Date().toISOString() +
+  ": " +
   "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function (c) {
-    var r = (Math.random() * 16) | 0,
-      v = c == "x" ? r : (r & 0x3) | 0x8
+    let r = (Math.random() * 16) | 0
+    let v = c == "x" ? r : (r & 0x3) | 0x8
     return v.toString(16)
   })
 
-let hash = genHash()
+let CURRENT_HASH = genHash()
+
+app.get("/status", (req, res) => {
+  res.send(CURRENT_HASH)
+})
+
+app.listen(port)
 
 const printHash = () => {
-  console.log(new Date().toISOString() + ": " + hash)
+  CURRENT_HASH = genHash()
+  console.log(CURRENT_HASH)
   setTimeout(printHash, 5000)
 }
 
