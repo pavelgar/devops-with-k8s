0a6390547715ffdec817762dd3f52248a7f94999
Pavel Garmuyev <pavel.garmuyev@gmail.com>
2021-07-10 15:00:29 +0300

    Add ping count to main app

--- main_app/frontend/index.js
+++ main_app/frontend/index.js
@@ -6,12 +6,16 @@ const app = new Koa()
 const PORT = process.env.PORT || 3000
 
 const directory = path.join("/", "usr", "src", "app", "files")
-const filePath = path.join(directory, "timestamp.txt")
+const timestampFile = path.join(directory, "timestamp.txt")
+const pingFile = path.join(directory, "pings.txt")
 
 app.use(async (ctx) => {
   if (ctx.path.includes("favicon.ico")) return
 
-  ctx.body = fs.readFileSync(filePath, "utf8")
+  ctx.body =
+    fs.readFileSync(timestampFile, "utf8") +
+    "\nPing / Pongs: " +
+    fs.readFileSync(pingFile, "utf8")
   ctx.set("Content-type", "text/plain")
   ctx.status = 200
 })
