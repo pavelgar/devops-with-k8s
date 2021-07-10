b5af7164df982b515e05f2961ba5866b5397e523
Pavel Garmuyev <pavel.garmuyev@gmail.com>
2021-07-10 17:23:31 +0300

    Switch back to express in ping-pong app

--- ping_app/index.js
+++ ping_app/index.js
@@ -1,20 +1,22 @@
-const Koa = require("koa")
+const express = require("express")
 const path = require("path")
 const fs = require("fs")
 
-const app = new Koa()
+const app = express()
 const PORT = process.env.PORT || 3000
 
 const directory = path.join("/", "usr", "src", "app", "files")
 const filePath = path.join(directory, "pings.txt")
 let ping_count = 0
 
-app.use(async (ctx) => {
-  if (ctx.path.includes("favicon.ico")) return
-  ctx.body = `pong ${ping_count}`
+app.get("/", (req, res) => {
+  res.send(`pong ${ping_count}`)
   fs.writeFileSync(filePath, ++ping_count + "")
 })
 
 fs.mkdirSync(directory, { recursive: true })
 fs.writeFileSync(filePath, ping_count + "")
-app.listen(PORT)
+
+app.listen(PORT, () => {
+  console.log(`Server started in port ${PORT}`)
+})
