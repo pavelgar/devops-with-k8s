1f3e5ea996069b6fc16ddba4486d83e3ea0d69af
Pavel Garmuyev <pavel.garmuyev@gmail.com>
2021-07-10 14:55:42 +0300

    Update ping app to save ping count to file

--- ping_app/index.js
+++ ping_app/index.js
@@ -1,13 +1,24 @@
-const express = require("express")
+const Koa = require("koa")
+const path = require("path")
+const fs = require("fs")
 
-const app = express()
-const port = process.env.PORT || 3000
+const app = new Koa()
+const PORT = process.env.PORT || 3000
 
+const directory = path.join("/", "usr", "src", "app", "files")
+const filePath = path.join(directory, "pings.txt")
 let ping_count = 0
 
-app.get("/", (req, res) => {
-  res.send(`pong ${ping_count}`)
-  ping_count++
+const savePing = (n) =>
+  fs.writeFile(filePath, n, (err) => {
+    if (err) throw err
+  })
+
+app.use(async (ctx) => {
+  if (ctx.path.includes("favicon.ico")) return
+  ctx.body = `pong ${ping_count}`
+  savePing(++ping_count)
 })
 
-app.listen(port)
+savePing(ping_count)
+app.listen(PORT)
