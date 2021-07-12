442df70dbc038f2b0c1c9a4db4efb164adeb0a32
Pavel Garmuyev <pavel.garmuyev@gmail.com>
2021-07-12 15:19:55 +0300

    Debugging the controller #2

--- DIY_CRD/controller/index.js
+++ DIY_CRD/controller/index.js
@@ -167,9 +167,10 @@ const maintainStatus = async () => {
 
   const deployment_stream = new JSONStream()
 
-  deployment_stream.on("data", async ({ type, object }) => {
+  deployment_stream.on("data", async (res) => {
+    console.log(res)
+    const { type, object } = res
     // If it's not dummysite deployment, don't handle it
-    console.log(object)
     if (!object.metadata.labels.dummysite) return
     // Don't handle deleted deployments either
     if (type === "DELETED" || object.metadata.deletionTimestamp) return
